cbfcf3d5894740ee474dde06683ce228593544c4
tomkoom <tomash.sugint@gmail.com>
2022-07-04 02:11:48 +0300

    feat: set up .env, add api request

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const request = require("postman-request");
+require("dotenv").config();
 
-const url = "http://api.weatherstack.com/current";
+const weatherstackAPIKey = process.env.WEATHERSTACK_APIKEY;
+const url = `http://api.weatherstack.com/current?access_key=${weatherstackAPIKey}&query=New%20York`;
+
+request({ url: url }, (error, response) => {
+	console.log(weatherstackAPIKey);
+	const data = JSON.parse(response.body);
+	console.log(data);
+});
