b1bb03000fe3270fc71e7e9e09acc68b400c1b6b
tomkoom <tomash.sugint@gmail.com>
2022-07-05 13:50:17 +0300

    feat: add error handling

--- src/app.js
+++ src/app.js
@@ -1,20 +1,39 @@
 const request = require("postman-request");
 require("dotenv").config();
 
+// apis
 const weatherstackAPIKey = process.env.WEATHERSTACK_APIKEY;
 const mapboxAPIKey = process.env.MAPBOX_APIKEY;
-const weatherstackURL = `http://api.weatherstack.com/current?access_key=${weatherstackAPIKey}&query=New%20York`;
-const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapboxAPIKey}&limit=1`;
+
+const weatherstackURL = `http://api.weatherstack.com/current?access_key=${weatherstackAPIKey}&query=37.8267,-122.4233`;
+// const weatherstackURL = `http://api.weatherstack.com/current?access_key=${weatherstackAPIKey}&query=New%20York`;
 
 request({ url: weatherstackURL, json: true }, (error, response) => {
-	const data = response.body;
-	console.log(data.current);
+	// connection err
+	if (error) {
+		console.log("Unable to connect to weather service");
+
+		// response err
+	} else if (response.body.error) {
+		console.log("Unable to find location (weatherstack)");
+	} else {
+		const data = response.body;
+		console.log(data.current);
+	}
 });
+
+const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapboxAPIKey}&limit=1`;
 
 // geocode request
 request({ url: mapboxURL, json: true }, (error, response) => {
-	const data = response.body;
-	const latitude = data.features[0].center[1];
-	const longitude = data.features[0].center[0];
-	console.log(latitude, longitude);
+	if (error) {
+		console.log("Unable to connect to geolocation service");
+	} else if (response.body.features.length === 0) {
+		console.log("Unable to find location (mapbox). Try another search");
+	} else {
+		const data = response.body;
+		const latitude = data.features[0].center[1];
+		const longitude = data.features[0].center[0];
+		console.log(latitude, longitude);
+	}
 });
