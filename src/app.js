b9ed59debb81cd6f072b0f1c4154bd13902b56fe
tomkoom <tomash.sugint@gmail.com>
2022-07-11 23:25:33 +0300

    refactor: rm comments

--- src/app.js
+++ src/app.js
@@ -4,16 +4,15 @@ const forecast = require("./utils/forecast");
 const address = process.argv[2];
 
 if (!address) {
-	console.log("Please provide an address");
+	console.log("Please provide an address.");
 } else {
 	geocode(address, (error, { latitude, longitude, location } = {}) => {
-		// default is emty object
-
 		if (error) {
 			return console.log(error);
 		}
-		// first latitude
-		// second longitude
+
+		// 1 latitude
+		// 2 longitude
 		forecast(latitude, longitude, (error, forecastData) => {
 			if (error) {
 				return console.log(error);
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,10 @@ require("dotenv").config();
 // apikey
 const weatherstackAPIKey = process.env.WEATHERSTACK_APIKEY;
 
-// const url = `http://api.weatherstack.com/current?access_key=${weatherstackAPIKey}&query=37.8267,-122.4233`;
-// const url = `http://api.weatherstack.com/current?access_key=${weatherstackAPIKey}&query=New%20York`;
-
 const forecast = (latitude, longitude, callback) => {
 	const url = `http://api.weatherstack.com/current?access_key=${weatherstackAPIKey}&query=${
 		latitude + "," + longitude
-	}&units=m`; // &units=m - celsius
+	}&units=m`; // &units=m = celsius
 	request({ url, json: true }, (error, { body }) => {
 		if (error) {
 			callback("Unable to connect to weather service.", undefined);
